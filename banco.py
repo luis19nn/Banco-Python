55837c9516e0198c0960c7a0672b6d93ffa33f92
luis.ferreira <luis.ferreira@smartgreen.net>
2022-06-24 14:04:03 -0300

    ADD funções para o banco

--- banco.py
+++ banco.py
@@ -13,31 +13,142 @@ def main() -> None:
 
 
 def menu() -> None:
-    pass
+    print('===================================')
+    print('=============== ATM ===============')
+    print('============ Luis BANK ============')
+    print('===================================')
+
+    print('Selecione uma opção abaixo: ')
+    print('1 - Criar Conta')
+    print('2 - Efetuar Saque')
+    print('3 - Efetuar Depósito')
+    print('4 - Efetuar Transferência')
+    print('5 - Listar Contas')
+    print('6 - Sair do Sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        criar_conta()
+    elif opcao == 2:
+        efetuar_saque()
+    elif opcao == 3:
+        efetuar_deposito()
+    elif opcao == 4:
+        efetuar_transferencia()
+    elif opcao == 5:
+        listar_contas()
+    elif opcao == 6:
+        print('Volte sempre!')
+        sleep(2)
+        exit(0)
+    else:
+        print('Opção inválida!')
+        menu()
 
 
 def criar_conta() -> None:
-    pass
+    print('Informe os dados do cliente: ')
+
+    nome: str = input('Nome do cliente: ')
+    email: str = input('E-mail do cliente: ')
+    cpf: str = input('CPF do cliente: ')
+    data_nascimento: str = input('Data de nascimento do cliente: ')
+    
+    cliente: Cliente = Cliente(nome, email, cpf, data_nascimento)
+
+    conta: Conta = Conta(cliente)
+
+    contas.append(conta)
+
+    print('Conta criada com sucesso!')
+    print('Dados da conta: ')
+    print('-------------------------')
+    print(conta)
+    sleep(2)
+    menu()
 
 
 def efetuar_saque() -> None:
-    pass
+    if len(contas) > 0:
+        numero: int = int(input('Informe o número da sua conta: '))
+        conta: Conta = buscar_conta_por_numero(numero)
+
+        if conta:
+            valor: float = float(input('Informe o valor do saque'))
+            conta.sacar(valor)
+        else:
+            print(f'Não foi encontrado a conta com o número {numero}')
+    else:
+        print('Ainda não existem contas cadastradas!')
+    
+    sleep(2)
+    menu()
 
 
 def efetuar_deposito() -> None:
-    pass
+    if len(contas) > 0:
+        numero: int = int(input('Informe o número da sua conta: '))
+        conta: Conta = buscar_conta_por_numero(numero)
+
+        if conta:
+            valor: float = float(input('Informe o valor do depósito: '))
+            conta.depositar(valor)
+        else:
+            print(f'Não foi encontrado a conta com o número {numero}')
+    else:
+        print('Ainda não existem contas cadastradas!')
+    
+    sleep(2)
+    menu()
 
 
 def efetuar_transferencia() -> None:
-    pass
+    if len(contas) > 0:
+        numero_o: int = int(input('Informe o número da sua conta: '))
+        conta_o: Conta = buscar_conta_por_numero(numero_o)
+
+        if conta_o:
+            numero_d: float = float(input('Informe o numero da conta destino: '))
+            conta_d: Conta = buscar_conta_por_numero(numero_d)
+
+            if conta_d:
+                valor: float = float(input('Informe o valor da transferência: '))
+                conta_o.transferir(conta_d, valor)
+            else:
+                print(f'Não foi encontrado a conta destino com o número {numero_d}')
+        else:
+            print(f'Não foi encontrado a sua conta com o número {numero_o}')
+    else:
+        print('Ainda não existem contas cadastradas!')
+    
+    sleep(2)
+    menu()
 
 
 def listar_contas() -> None:
-    pass
+    if len(contas) > 0:
+        print('Listagem de Contas')
+
+        for conta in contas:
+            print(conta)
+            print('-----------------------')
+            sleep(1)
+    else:
+        print('Ainda não existem contas cadastradas!')
+    
+    sleep(2)
+    menu()
 
 
-def buscar_conta_por_numero() -> None:
-    pass
+def buscar_conta_por_numero(numero: int) -> None:
+    c: Conta = None
+
+    if len(contas) > 0:
+        for conta in contas:
+            if conta.numero == numero:
+                c = conta
+    return c
 
 
 if __name__ == '__main__':
